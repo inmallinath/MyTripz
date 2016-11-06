94410c08932f9ad10eb8bc8fa77bdcebc9dcfa60
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-11-06 08:40:20 -0800

    Add Views and Controllers for Index

--- src/MyTripz/Startup.cs
+++ src/MyTripz/Startup.cs
@@ -17,6 +17,7 @@ namespace MyTripz
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,14 @@ namespace MyTripz
             //    await context.Response.WriteAsync("Hello World!");
             //});
             app.UseStaticFiles();
+
+            app.UseMvc(config => {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" }
+                    );
+            });
         }
     }
 }
