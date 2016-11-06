ec95c8a35db135c190a9e342f8027387853bd0b0
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-11-06 07:12:12 -0800

    Add JS and jQuery to add some dynamism

---
+++ src/MyTripz/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Site.JS
+(function () {
+
+    var element = $("#username");
+    element.text("Mally Igoor");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.style = "background-color: #888;";
+    });
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    });
+ })();
