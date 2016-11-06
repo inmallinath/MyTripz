0c20443386270eb7c76ea77311cc2d66cc5579d6
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-11-06 07:38:07 -0800

    More Jquery

--- src/MyTripz/wwwroot/js/site.js
+++ src/MyTripz/wwwroot/js/site.js
@@ -1,20 +1,32 @@
 ﻿// Site.JS
 (function () {
 
-    var element = $("#username");
-    element.text("Mally Igoor");
+    //var element = $("#username");
+    //element.text("Mally Igoor");
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.style = "background-color: #888;";
-    });
-    main.on("mouseleave", function () {
-        main.style = "";
-    });
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
 
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function () {
-        var me = $(this);
-        alert(me.text());
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        }
+        else {
+            $(this).text("Hide Sidebar");
+        }
     });
  })();
