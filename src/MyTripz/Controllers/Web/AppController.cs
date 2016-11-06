31cd12b1832822c425179a8232551db473e5c6e8
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-11-06 08:29:17 -0800

    Create Controller and View Folders

---
+++ src/MyTripz/Controllers/Web/AppController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTripz.Controllers.Web
+{
+    public class AppController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
